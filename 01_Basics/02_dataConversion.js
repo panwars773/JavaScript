a5c1842b97f2e0c7676f0024ed148b98636a405f
panwars773 <panwars773@gmail.com>
2026-07-13 12:36:55 +0000

    DataTypes

---
+++ 01_Basics/02_dataConversion.js
@@ -0,0 +1,7 @@
+let Score="i"
+//console.log(typeof Score)
+let ScoreString=Number(Score)
+//console.log(typeof ScoreString)
+//console.log(ScoreString)
+let booleanScore=Boolean(Score)
+console.log(booleanScore)
