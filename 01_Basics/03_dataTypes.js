78dca9ab149d722b1a4cf9ca7175fb779108715b
panwars773 <panwars773@gmail.com>
2026-07-14 09:41:48 +0000

    DataTypes

---
+++ 01_Basics/03_dataTypes.js
@@ -0,0 +1,7 @@
+let myArray=[1,"Shubham",34,false]
+let myFunction=function(){}
+let myObject={name:"Shubham", age:29}
+
+console.log(typeof myArray)
+console.log(typeof myFunction)
+console.log(typeof myObject)
