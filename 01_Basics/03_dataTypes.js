26898d713a788df8b832c2c82dbc007855366d2e
panwars773 <panwars773@gmail.com>
2026-07-14 16:44:04 +0000

    Strings

--- 01_Basics/03_dataTypes.js
+++ 01_Basics/03_dataTypes.js
@@ -1,7 +1,9 @@
 let myArray=[1,"Shubham",34,false]
 let myFunction=function(){}
 let myObject={name:"Shubham", age:29}
+let mySymbol=Symbol(9879)
 
 console.log(typeof myArray)
 console.log(typeof myFunction)
 console.log(typeof myObject)
+console.log(typeof mySymbol)
---
+++ 01_Basics/04_strings.js
@@ -0,0 +1,8 @@
+let name="Shubham",age=29,city="Ambala"
+
+console.log(`My name is ${name} and I am ${age} years old. I live in ${city}`)
+console.log(name[1])
+console.log(name.length);
+console.log(name.toUpperCase())
+console.log(name.charAt(5))
+console.log(name.indexOf("h"))
